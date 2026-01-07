e7abfd8040e843669029e72707bfa2e1c601cca7
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-01-07 11:45:02 +0530

    Product Details - Single Product Functionality and link all product to specific product. imp*** logic

--- store/views.py
+++ store/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 
+
 def store(request, category_slug=None):
     categories = None
-    products = None 
+    products = None
 
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
@@ -16,3 +17,13 @@ def store(request, category_slug=None):
     context = {"products": products, "count": count}
 
     return render(request, "store/store.html", context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)  # __ to get access to that model of field
+    except Exception as e:
+        raise e
+    
+    context = { "single_product":single_product,}
+    return render(request, "store/product_detail.html", context)
