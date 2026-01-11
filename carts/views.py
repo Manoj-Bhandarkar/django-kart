66f321e0b396c9c80d6100189505c40d674ab8e3
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-01-11 09:41:27 +0530

    fix

--- carts/views.py
+++ carts/views.py
@@ -32,20 +32,20 @@ def add_cart(request, product_id):
     return redirect("cart")
 
 def remove_cart(request,product_id):
-    cart = Cart.object.get(cart_id=_cart_id(request))
+    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.object.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
     if cart_item.quantity > 1:
         cart_item.quantity -= 1
         cart_item.save()
-    else
+    else:
         cart_item.delete()
     return redirect('cart')
 
 def delete_cart_item(request,product_id):
-    cart = Cart.object.get(cart_id=_cart_id(request))
+    cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.object.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
     cart_item.delete()
     return redirect('cart')
 
