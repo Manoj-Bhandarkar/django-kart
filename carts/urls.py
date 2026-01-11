500b6c6f32e97087b1f747c5941f8cbf4c2642e4
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-01-11 08:03:30 +0530

    Cart - Decrement & Remove Cart Items

--- carts/urls.py
+++ carts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.cart, name="cart"),
-    path("add_cart/<int:product_id>/", views.add_cart, name='add_cart')
+    path("add_cart/<int:product_id>/", views.add_cart, name='add_cart'),
+    path("add_cart/<int:product_id/", views.remove_cart, name='remove_cart'),
+     path("add_cart/<int:product_id/", views.delete_cart_item, name='delete_cart_item')
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from store.models import Product
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
@@ -31,6 +31,23 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect("cart")
 
+def remove_cart(request,product_id):
+    cart = Cart.object.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.object.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else
+        cart_item.delete()
+    return redirect('cart')
+
+def delete_cart_item(request,product_id):
+    cart = Cart.object.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.object.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_item=None):
     try:
