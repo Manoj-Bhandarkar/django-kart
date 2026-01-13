8fde1328f6ebac418b9c2e42381292314037045c
manoj-bhandakar <developer.manojbhandarkar@gmail.com>
2026-01-13 22:21:17 +0530

    Variation - Get the Instance of Variation

--- carts/models.py
+++ carts/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 
 class Cart(models.Model):
     cart_id = models.CharField(max_length=250, blank=True, null=True)
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.cart_id
+        return str(self.cart_id)
     
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation = models.ManyToManyField(Variation, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
@@ -19,4 +20,4 @@ class CartItem(models.Model):
         return self.product.price * self.quantity
 
     def __str__(self):
-        return self.product
+        return str(self.product)
